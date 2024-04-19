383063ac29c0efd5cd408aeac48b6804fad82861
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-04-19 15:41:58 -0600

    Initial commit

---
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -0,0 +1,6 @@
+package com.inn.cafe.constants;
+
+public class CafeConstants {
+    public static final String SOMETHING_WENT_WRONG= "Something went wrong.";
+    public static final String INVALID_DATA = "Invalid Data.";
+}
